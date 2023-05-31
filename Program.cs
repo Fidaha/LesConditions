6ddbb6fef093adcc10881f903047b3c20f512f39
Fidaha <81554525+Fidaha@users.noreply.github.com>
2023-05-31 11:09:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LesConditions;
+using System;
+using System.IO; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    // Fais saisir à l'utilisateur deux valeurs entières : un minimum et un maximum.
+    // Fais saisir un troisième entier à l'utilisateur et borne-le au maximum et au minimum.
+
+    int min, max, val;
+
+        Console.WriteLine("Entrez la valeur minimale : ");
+        min = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entrez la valeur maximale : ");
+        max = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entrez une valeur : ");
+        val = int.Parse(Console.ReadLine());
+
+if (val < min)
+{
+    val = min;
+}
+else if (val > max)
+{
+    val = max;
+}
+
+        Console.WriteLine("La valeur saisie est : " + val);
+
+
+
+        
+    }
+}
